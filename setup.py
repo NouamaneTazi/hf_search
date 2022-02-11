edcb2d29bff04858b2b9b3d2d5043159f5cd544e
Nouamane Tazi <nouamane98@gmail.com>
2022-02-11 18:21:33 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,24 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='hf_search',
-    version='0.1',
+    name="hf_search",
+    version="0.1",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    url='https://github.com/nouamanetazi/hf_search',
-    license='mit',
-    author='nouamanetazi',
-    author_email='nouamane98@gmail.com',
-    description='hf semantic search'
+    url="https://github.com/nouamanetazi/hf_search",
+    license="MIT",
+    author="nouamanetazi",
+    author_email="nouamane98@gmail.com",
+    description="hf semantic search",
+    install_requires=[
+        "huggingface_hub==0.4.0",
+        "joblib",
+        "numpy",
+        "pandas",
+        "rank_bm25",
+        "scikit_learn",
+        "sentence_transformers",
+        "setuptools",
+        "tqdm",
+    ],
 )
